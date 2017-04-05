cd18e41416a9630e97451bb5819dd2965b90175b
DESKTOP-G3UB1RN\Ruby Simpson <rubysimpsonwork@gmail.com>
2017-04-05 20:49:16 +1000

    Put in a text display for the livesController

--- EGONE Unity Project/Assets/Scripts/LivesController.cs
+++ EGONE Unity Project/Assets/Scripts/LivesController.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LivesController : MonoBehaviour {
 
     int playerLives = 3;
     public AudioSource deathSound;
+    public Text livesDisplay;
+
+    //start void
+    void Start()
+    {
+        livesDisplay.text = "Lives: " + playerLives;
+    }
 
 	// Update is called once per frame
 	void Update () {
@@ -18,13 +26,27 @@ public class LivesController : MonoBehaviour {
     void Death()
     {
         //play death animation
+
         //play death sfx
         deathSound.Play();
+
         //restart at beginning of level
+        Restart();
     }
 
     public void LoseLife()
     {
         playerLives -= 1;
+        livesDisplay.text = "Lives: " + playerLives;
+    }
+
+    void Restart()
+    {
+        //player restarts at the start of the level
+
+        //playerLives reset to max
+        //playerLives = 3;
+
+        //actually....is there a way to just reload the level from the start? just do that
     }
 }
