3c218773b8a238e70472f8457c112b56d70d2bf4
SAE\bHoughton <benhoughta@hotmail.com>
2017-04-05 14:48:47 +1000

    Started trying to create the balancing mechanic for the head.

---
+++ EGONE Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private float speed = 10.0f;
+
+    Rigidbody2D RB;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        RB.AddForce(new Vector2(x,0));
+        //transform.Translate (x, 0, 0);
+       // transform.Translate(0, 0, z);
+	}
+}
