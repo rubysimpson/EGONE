ab331cdbecd0e389c1dd67406550d4cd6781ad63
SAE\rsimpson2 <rubysimpsonwork@gmail.com>
2017-04-05 12:38:39 +1000

    Made a generalfuckery scene so I can mess around in it and not damage other people's work.  Started writing and implementing the boast script including making the textbox prefab (not finished yet)

---
+++ EGONE Unity Project/Assets/Scripts/BoastScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoastScript : MonoBehaviour {
+
+    public Text boastTextBox;
+    public string[] boastPhrases;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if() boastbutton then chck npcproximity
+	}
+
+    void CheckNPCProximity()
+    {
+        //If NPC (check tag) is within x dis of the direction the player is facing the player can boast to them
+    }
+
+    void Boast()
+    {
+        //boast animation
+        //boast sound
+        //boast bark
+        //win or lose
+    }
+
+    void BoastBark()
+    {
+        //select phrase
+        //put phrase in textbox
+        //delete after x
+    }
+}
