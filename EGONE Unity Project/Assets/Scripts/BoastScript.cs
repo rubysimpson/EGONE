ae440d31a4b5cdcc3e905123c85aa693eec666c2
DESKTOP-G3UB1RN\Ruby Simpson <rubysimpsonwork@gmail.com>
2017-04-05 19:08:56 +1000

    Added SFXs to the Boast Script

--- EGONE Unity Project/Assets/Scripts/BoastScript.cs
+++ EGONE Unity Project/Assets/Scripts/BoastScript.cs
@@ -5,40 +5,58 @@ using UnityEngine.UI;
 
 public class BoastScript : MonoBehaviour {
 
+    //Bark variables
     public Text boastTextBox;
     public string[] boastPhrases;
-    public AudioSource boastSound;
     public Canvas BoastCanvas;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    //Audio variables
+    public AudioSource boastSound;
+    public AudioSource winBoastSound;
+    public AudioSource loseBoastSound;
+
 	// Update is called once per frame
 	void Update () {
         //if() boastbutton then chck npcproximity
         if (Input.GetKeyDown("space"))
         {
             //CheckNPCProximity();
-            BoastBark();
+            Boast();
         }
 	}
 
     public void CheckNPCProximity()
     {
         //If NPC (check tag) is within x dis of the direction the player is facing the player can boast to them
-
+        //get someone to help with this probably
     }
 
     public void Boast()
     {
         //boast animation
+
         //boast sound
         boastSound.Play(); //i don't know if this will work - doublecheck
+        
         //boast bark
         BoastBark();
+        
         //win or lose
+        /*if(playerHeadLevel? > NPCHeadLevel)
+        {
+            //sendmessage to npc to play npc lose animation
+            //sendmessage to playerHeadLevel to +1
+            //play player boast win animation
+            
+            //play win sfx
+            winBoastSound.Play(5); //5 is how long it might take for the initial grunt and animation?
+        }
+        else{
+            //send message for player to lose a life
+            //play player lose boast animation
+            //play player lose sfx
+            loseBoastSound.Play(5);
+        }*/
     }
 
     public void BoastBark()
