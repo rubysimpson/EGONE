0dc3f44c4f9f5198e681df6bc18ee4aff9c6af18
SAE\rsimpson2 <rubysimpsonwork@gmail.com>
2017-04-10 12:37:15 +1000

    Made NPC prefab and started on NPCController script

---
+++ EGONE Unity Project/Assets/Scripts/NPCController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    bool beenBoasted = false;
+
+    void FacePlayer()
+    {
+        //if npc is facing away from player when player uses boast function they will turn to face them
+    }
+
+    void SitBackDown()
+    {
+        //play lose boast animation - called when player wins a boast
+    }
+}
