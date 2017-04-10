1d37862f553264f41367e0595aa51f5191731a2d
SAE\rsimpson2 <rubysimpsonwork@gmail.com>
2017-04-10 14:25:37 +1000

    Made a HeadLevel Scriptable Object and am in the process of filling it out/figuring out how to get the player to reference it correctly.  Writing a HeadLevel Tech Spec

---
+++ EGONE Unity Project/Assets/Scripts/Data Makers/HeadLevelData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class HeadLevelData : ScriptableObject {
+
+    public HeadVariables[] headLevel;
+
+    [System.Serializable]
+    public class HeadVariables
+    {
+        //variables for level, scale, mass, linear drag
+        public int level;
+        public Vector3 scale;
+        public float mass;
+        public float drag;
+    }
+}
