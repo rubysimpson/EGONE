a41ec2a5952bab6cc89ac36043e230a6ea8c40c9
SAE\bHoughton <benhoughta@hotmail.com>
2017-04-10 14:37:09 +1000

    Created a new script HeadMotorController and changed Menu scene to Level

---
+++ EGONE Unity Project/Assets/HeadMotorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMotorController : MonoBehaviour {
+
+    public GameObject head;
+    HingeJoint2D HJ;
+
+	// Use this for initialization
+	void Start () {
+        HJ = GetComponent<HingeJoint2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
