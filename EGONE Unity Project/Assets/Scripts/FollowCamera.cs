964088f24c6fca03499a63db6cd5e83aaf7254ae
SAE\bHoughton <benhoughta@hotmail.com>
2017-04-12 12:08:31 +1000

    Fixed Movement - player now falls over

---
+++ EGONE Unity Project/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0, 4, -2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
