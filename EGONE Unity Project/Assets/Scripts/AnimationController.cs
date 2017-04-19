0f73c11bc11ff461d9890508225462d0bf7932a4
SAE\mvonwilpert <1004624@student.sae.edu.au>
2017-04-19 12:43:45 +1000

    Did a whole bunch of shit. No time to explain, playtesting soon.

---
+++ EGONE Unity Project/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	public bool IsMoving;
+	public bool BoastWin;
+	public bool BoastFail;
+	public bool Idle;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Animation(){
+		if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.D))
+			IsMoving = true;
+		if (Input.GetKeyDown(KeyCode.Space))
+			BoastWin = true;
+	}
+}
