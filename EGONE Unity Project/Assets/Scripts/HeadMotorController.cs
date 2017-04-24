6fa46efc116c97d7424b5945cb8771fad2bcf81b
SAE\bHoughton <benhoughta@hotmail.com>
2017-04-24 12:58:23 +1000

    Steve helped me add math to the head so now it tries to stand up on it's own. Lots of Math.

--- EGONE Unity Project/Assets/Scripts/HeadMotorController.cs
+++ EGONE Unity Project/Assets/Scripts/HeadMotorController.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class HeadMotorController : MonoBehaviour {
 
-    public GameObject head;
-    HingeJoint2D hinge;
-
-    // Use this for initialization
-    void Start ()
-    {
-        hinge = GetComponent<HingeJoint2D>();
-      
-       
-    }
+    public Transform head, body;
+    public Rigidbody2D hingeRB;
+    public HingeJoint2D hinge;
+    public float motorSpeedScale = 1;
+    public AnimationCurve curve = AnimationCurve.Linear(0,0,1,1);
+    
 	
 	// Update is called once per frame
 	void Update ()
     {
-        TiltUp ();
-	}
+        var max = hinge.limits.max;
+        var ang = Mathf.Cos(max * Mathf.Deg2Rad);
+        var upAng = Vector3.Dot(body.up, head.up);
+        upAng = 1 - upAng;
+        var torque = upAng / ang ;
+        torque = Mathf.Clamp(curve.Evaluate(torque),0,1);
 
-    void TiltUp ()
-    {
-        //somehow find a way to rotate the head upward
+        var leftOrRight = Vector3.Dot(head.transform.up, body.right);
+
+        leftOrRight = leftOrRight > 0 ? 1 : -1;
+
+        torque *= leftOrRight;
+        torque *= motorSpeedScale;
+
+        hingeRB.AddTorque(torque, ForceMode2D.Impulse);
+        Debug.DrawRay(transform.position, torque * Vector3.right);
     }
-
-   
 }
