7264387e2e5613b710c005aa8252e127e12272cd
SAE\bHoughton <benhoughta@hotmail.com>
2017-04-24 13:57:21 +1000

    Fixed the scenes so thata they load the next level when the play wins and loads the same level when  they player looses.

--- EGONE Unity Project/Assets/Scripts/LoseScreen.cs
+++ EGONE Unity Project/Assets/Scripts/LoseScreen.cs
@@ -8,29 +8,12 @@ public class LoseScreen : MonoBehaviour {
 	public PlayerController playControl;
 
 	void Start(){
-		playControl = GameObject.FindGameObjectWithTag ("PlayerBody").GetComponent<PlayerController> ();
 	}
 
 	void Update(){
-		WinLoose ();
-	}
-
-	public void WinLoose(){
-		if (playControl.enabled == false && Input.GetKeyDown(KeyCode.Space))
-            // && playerLoose == true //This needs to go into the WinLoose If Statement above so if the player has lost they reload the same level.
+		if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(1);
-            playControl.enabled = true;
-            //This needs to have something that grabs the current scene and loads that.
-            
-		}
-        if (playControl.enabled == false && Input.GetKeyDown(KeyCode.Space))
-        // && playerWon == true //This needs to go into the WinLoose If Statement above so if the player has won they load the next level.
-        {
-            SceneManager.LoadScene(1);
-            playControl.enabled = true;
-            //This needs to have something that automatically selects the next level based on the current level.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-
-    }
+	}
 }
